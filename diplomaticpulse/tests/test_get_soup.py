44c2cd60a7ad12e6dc74b85c58aef412d600ec17
alattab <kader.lattab@gmail.com>
2022-02-16 10:04:26 +0300

    add --no-sandbox to test_get_soup

--- diplomaticpulse/tests/test_get_soup.py
+++ diplomaticpulse/tests/test_get_soup.py
@@ -30,6 +30,7 @@ class TestHtmlUtils(unittest.TestCase):
         url = "http://www.google.com"
         options = Options()
         options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
         options.add_argument("--disable-gpu")  # Last I checked this was necessary.
         driver = webdriver.Chrome(chrome_options=options)
         result = util.get_soup(url, driver)

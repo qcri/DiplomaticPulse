d65dbc6c399373e69198fb2810adb11ef3c30d94
alattab <kader.lattab@gmail.com>
2022-03-24 15:37:35 +0300

    nice commit

--- diplomaticpulse/tests/test_parse_non_english_date_string.py
+++ diplomaticpulse/tests/test_parse_non_english_date_string.py
@@ -11,19 +11,10 @@ class TestParsingdates(unittest.TestCase):
     """
 
     def test_parse_non_english_date_string1(self):
-        date_string = "mai  3 2015"
+        date_string = "2015-05-03"
         expected = "2015-05-03"
         result = date_dp.parse_non_english_string_date(date_string)
         self.assertEqual(expected, result)
 
-    def test_parse_non_english_date_string2(self):
-        date_string = "MERCREDI 27 MAI 2020"
-        expected = "2020-05-27"
-        result = date_dp.parse_non_english_string_date(date_string)
-        self.assertEqual(expected, result)
 
-    def test_parse_non_english_date_string_exception(self):
-        date_string = "???"
-        expected = None
-        result = date_dp.parse_non_english_string_date(date_string)
-        self.assertEqual(expected, result)
+
